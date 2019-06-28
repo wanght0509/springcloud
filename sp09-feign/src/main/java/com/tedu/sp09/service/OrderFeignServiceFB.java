3431ebb88b2f171f5fc31a09bb5228eea5029463
wanght0509 <2068863285@qq.com>
2019-06-28 09:16:55 +0800

    a

---
+++ sp09-feign/src/main/java/com/tedu/sp09/service/OrderFeignServiceFB.java
@@ -0,0 +1,18 @@
+package com.tedu.sp09.service;
+
+import com.tedu.sp01.pojo.Order;
+import com.tedu.web.util.JsonResult;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderFeignServiceFB implements OrderFeignService {
+    @Override
+    public JsonResult<Order> getOrder(String orderId) {
+        return JsonResult.err("无法获取商品订单");
+    }
+
+    @Override
+    public JsonResult addOrder() {
+        return JsonResult.err("无法保存订单");
+    }
+}
